23c21084018549898ae76dd27dddf6befa21653c
DEV1\Leandro <leandro.sardi@expandedventure.com>
2022-01-27 14:19:37 -0300

    building full automation

--- sh/ipv6.install.2.sh
+++ sh/ipv6.install.2.sh
@@ -1,5 +1,66 @@
-PREFIX = $1
-echo "Prefix is: " . $PREFIX 
 
-PORT = $2
-echo "Port is: " . $PORT 
+#!/bin/bash
+# Naked variables
+
+prefix=$1
+port=$2
+
+echo "Prefix is: $prefix" 
+echo "Port is: $port"
+
+ran=( a b c d e f 1 2 3 4 5 6 7 8 9 0 )
+nip=50
+ind=1
+apt-get install ethtool
+apt-get install curl
+rm /etc/ipv6tmp
+#interface=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | head -n1 | awk '{print $1;}')
+interface=$(ip -o -4 route show to default | awk '{print $5}' | sed 1'!d')
+sleep 1
+mainIP=$(curl -s http://ipinfo.io/ip)
+
+sh /usr/local/etc/3proxy/scripts/rc.d/proxy.sh stop
+
+ifconfig $interface add $prefix::/48
+ip -6 route add default via $prefix::1
+
+echo "ifconfig $interface add $prefix::/48
+ip -6 route add default via $prefix::1" >> /etc/network.conf
+
+chmod +x /etc/rc.local
+chmod +x /etc/network.conf
+
+#REMOVE exit 0 FROM rc.local
+sed -i '/exit 0/d' /etc/rc.local
+echo "bash /etc/network.conf" >> /etc/rc.local
+
+while [ "$ind" -le $nip ]; do
+    a=${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}
+    b=${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}
+    c=${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}
+    d=${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}
+    e=${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}${ran[$RANDOM%16]}
+    echo proxy -p$port -6 -a -n  -i0.0.0.0 -e$prefix:$a:$b:$c:$d:$e >> /usr/local/etc/3proxy/cfg/3proxy.cfg
+    echo $mainIP:$port >> /etc/ipv6tmp
+    port=$((port+1))
+    ind=$((ind+1))
+    ip address add $prefix:$a:$b:$c:$d:$e dev $interface  > /dev/null 2>&1
+
+    echo "ip address add $prefix:$a:$b:$c:$d:$e dev $interface" >> /etc/network.conf
+    echo "$prefix:$a:$b:$c:$d:$e" >> /etc/network.ips
+
+done
+
+sh /usr/local/etc/3proxy/scripts/rc.d/proxy.sh start
+
+echo "exit 0" >> /etc/rc.local
+
+echo "Below are your new proxies:"
+cat /etc/ipv6tmp
+echo
+echo
+echo "Copy your proxies to another file, and make sure they are saved as they cannot be accessed again, highlight with your mouse and it'll be automatically copied."
+echo
+rm /etc/ipv6tmp
+sh /usr/local/etc/3proxy/scripts/rc.d/proxy.sh stop  > /dev/null 2>&1
+sh /usr/local/etc/3proxy/scripts/rc.d/proxy.sh start  > /dev/null 2>&1
